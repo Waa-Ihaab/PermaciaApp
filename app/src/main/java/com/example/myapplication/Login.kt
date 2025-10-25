c001f0757d3f1f32e582f8c7528d58bb8fd196d1
PermaciaApp <coronalmeryoul2020@gmail.com>
2025-10-25 15:51:21 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
